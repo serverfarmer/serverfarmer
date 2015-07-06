9faf4dbdfa67dcccb34ad8702f5ab2028e14f944
Tomasz Klim <github@tomaszklim.pl>
2015-07-06 17:44:33 +0200

    more automatic local backups

--- scripts/backup/cron-weekly.sh
+++ scripts/backup/cron-weekly.sh
@@ -7,13 +7,20 @@ DEST="/backup/weekly"
 
 backup_encrypt_directory $TMP $DEST /boot boot.tar
 backup_encrypt_directory $TMP $DEST /srv/apps/motion srv_apps_motion.tar
-backup_encrypt_directory $TMP $DEST /srv/sites/beta srv_sites_beta.tar
-backup_encrypt_directory $TMP $DEST /srv/sites/tomekwiki srv_sites_tomekwiki.tar
-backup_encrypt_directory $TMP $DEST /home/tomek home_tomek.tar
 
+for D in `ls /srv/sites`; do
+	if [ ! -f /srv/sites/$D/.nobackup ] && [ -f /srv/sites/$D/.weekly ]; then
+		backup_encrypt_directory $TMP $DEST /srv/sites/$D srv_sites_$D.tar
+	fi
+done
+
+for D in `ls /home`; do
+	if [ "`ls /home/$D/`" != "" ]; then
+		backup_encrypt_directory $TMP $DEST /home/$D home_$D.tar
+	fi
+done
 
 if [ -f /etc/mysql/debian.cnf ] && [ -f /var/run/mysqld/mysqld.pid ]; then
 	pass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
 	backup_mysql 127.0.0.1 3306 debian-sys-maint $pass mysql $TMP $DEST
 fi
-
