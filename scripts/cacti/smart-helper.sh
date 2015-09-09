e54515d891a69ba02bece3a1b25f7d54ccd30b72
Tomasz Klim <github@tomaszklim.pl>
2015-09-09 23:23:46 +0200

    rewritten smart reader script from php to shell

--- scripts/cacti/smart-helper.sh
+++ scripts/cacti/smart-helper.sh
@@ -3,5 +3,21 @@
 # crontab -e (jako root):
 #   */5 * * * * /opt/farm/scripts/cacti/smart-helper.sh
 
-/opt/farm/scripts/cacti/smart-dumper.php
+path="/var/cache/cacti"
 
+
+if [ "$1" = "--force" ]; then
+	disks=`ls /dev/disk/by-id/ata-* |grep -v -- -part |grep -v VBOX_HARDDISK |grep -v CF_CARD`
+else
+	disks=`ls /dev/disk/by-id/ata-* |grep -v -- -part |grep -v VBOX_HARDDISK |grep -v CF_CARD |grep -vxFf /opt/farm/common/standby.conf`
+fi
+
+for disk in $disks; do
+	device="`basename $disk`"
+	file="$path/$device.txt"
+
+	/usr/sbin/smartctl -d sat -T permissive -a $disk >$file.new
+	mv -f $file.new $file 2>/dev/null
+
+	/opt/farm/scripts/cacti/send.sh $file
+done
