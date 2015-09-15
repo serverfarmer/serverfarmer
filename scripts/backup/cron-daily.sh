1d6558c18c52cf4fa870aeba8ea94bcca903fc49
Tomasz Klim <github@tomaszklim.pl>
2015-09-15 08:38:55 +0200

    yet more automatic local backups

--- scripts/backup/cron-daily.sh
+++ scripts/backup/cron-daily.sh
@@ -6,13 +6,16 @@ TMP="`local_backup_directory`"
 DEST="$TMP/daily"
 
 backup_encrypt_directory $TMP $DEST /etc etc.tar
-backup_encrypt_directory $TMP $DEST /var/log var_log.tar
-backup_encrypt_directory $TMP $DEST /var/www var_www.tar
-backup_encrypt_directory $TMP $DEST /srv/apps/logs srv_apps_logs.tar
-backup_encrypt_directory $TMP $DEST /srv/apps/mfs srv_apps_mfs.tar
-backup_encrypt_directory $TMP $DEST /srv/apps/samba srv_apps_samba.tar
 backup_encrypt_directory $TMP $DEST /var/lib/mfs var_lib_mfs.tar
 backup_encrypt_directory $TMP $DEST /var/lib/samba var_lib_samba.tar
+backup_encrypt_directory $TMP $DEST /var/log var_log.tar
+backup_encrypt_directory $TMP $DEST /var/www var_www.tar
+
+for D in `ls /srv/apps`; do
+	if [ ! -f /srv/apps/$D/.nobackup ] && [ ! -f /srv/apps/$D/.weekly ]; then
+		backup_encrypt_directory $TMP $DEST /srv/apps/$D srv_apps_$D.tar
+	fi
+done
 
 for D in `ls /srv/sites`; do
 	if [ ! -f /srv/sites/$D/.nobackup ] && [ ! -f /srv/sites/$D/.weekly ]; then
--- scripts/backup/cron-weekly.sh
+++ scripts/backup/cron-weekly.sh
@@ -7,8 +7,12 @@ TMP="`local_backup_directory`"
 DEST="$TMP/weekly"
 
 backup_encrypt_directory $TMP $DEST /boot boot.tar
-backup_encrypt_directory $TMP $DEST /srv/apps/hosting srv_apps_hosting.tar
-backup_encrypt_directory $TMP $DEST /srv/apps/motion srv_apps_motion.tar
+
+for D in `ls /srv/apps`; do
+	if [ ! -f /srv/apps/$D/.nobackup ] && [ -f /srv/apps/$D/.weekly ]; then
+		backup_encrypt_directory $TMP $DEST /srv/apps/$D srv_apps_$D.tar
+	fi
+done
 
 for D in `ls /srv/sites`; do
 	if [ ! -f /srv/sites/$D/.nobackup ] && [ -f /srv/sites/$D/.weekly ]; then
