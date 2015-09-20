667bc82642edcf875c96f3ed2128cdc2fd7b457a
Tomasz Klim <github@tomaszklim.pl>
2015-09-20 09:44:03 +0200

    directories /srv/apps and /srv/sites are optional now

--- scripts/backup/cron-daily.sh
+++ scripts/backup/cron-daily.sh
@@ -11,14 +11,18 @@ backup_encrypt_directory $TMP $DEST /var/lib/samba var_lib_samba.tar
 backup_encrypt_directory $TMP $DEST /var/log var_log.tar
 backup_encrypt_directory $TMP $DEST /var/www var_www.tar
 
-for D in `ls /srv/apps`; do
-	if [ ! -f /srv/apps/$D/.nobackup ] && [ ! -f /srv/apps/$D/.weekly ]; then
-		backup_encrypt_directory $TMP $DEST /srv/apps/$D srv_apps_$D.tar
-	fi
-done
+if [ -d /srv/apps ]; then
+	for D in `ls /srv/apps`; do
+		if [ ! -f /srv/apps/$D/.nobackup ] && [ ! -f /srv/apps/$D/.weekly ]; then
+			backup_encrypt_directory $TMP $DEST /srv/apps/$D srv_apps_$D.tar
+		fi
+	done
+fi
 
-for D in `ls /srv/sites`; do
-	if [ ! -f /srv/sites/$D/.nobackup ] && [ ! -f /srv/sites/$D/.weekly ]; then
-		backup_encrypt_directory $TMP $DEST /srv/sites/$D srv_sites_$D.tar
-	fi
-done
+if [ -d /srv/sites ]; then
+	for D in `ls /srv/sites`; do
+		if [ ! -f /srv/sites/$D/.nobackup ] && [ ! -f /srv/sites/$D/.weekly ]; then
+			backup_encrypt_directory $TMP $DEST /srv/sites/$D srv_sites_$D.tar
+		fi
+	done
+fi
