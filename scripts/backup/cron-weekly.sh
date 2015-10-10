71071b308fd1df7b604ceb3d6791f45201b467e8
Tomasz Klim <github@tomaszklim.pl>
2015-10-10 23:48:15 +0200

    prototype version of postgres database backup functionality

--- scripts/backup/cron-weekly.sh
+++ scripts/backup/cron-weekly.sh
@@ -2,6 +2,7 @@
 . /opt/farm/scripts/functions.custom
 . /opt/farm/scripts/functions.backup
 . /opt/farm/scripts/functions.mysql
+. /opt/farm/scripts/functions.postgres
 
 TMP="`local_backup_directory`"
 DEST="$TMP/weekly"
@@ -21,4 +22,13 @@ done
 if [ -f /etc/mysql/debian.cnf ] && [ -f /var/run/mysqld/mysqld.pid ]; then
 	pass="`cat /etc/mysql/debian.cnf |grep password |tail -n1 |sed s/password\ =\ //g`"
 	backup_mysql 127.0.0.1 3306 debian-sys-maint $pass mysql $TMP $DEST
+fi
+
+if [ -x /usr/bin/psql ] && [ -x /usr/bin/pg_dump ]; then
+	dbs=`pg_list_local_databases`
+	for db in $dbs; do
+		fname="postgres-$db.sql.gpg"
+		sudo -u postgres pg_dump -c -i $db |gpg --encrypt --no-armor --recipient `gpg_backup_key` --output $TMP/$fname --batch
+		mv -f $TMP/$fname $DEST/$fname 2>/dev/null
+	done
 fi
