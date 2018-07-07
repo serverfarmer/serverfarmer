8818b83ba68d782a5e07d0682a2fef035f73cc3f
Tomasz Klim <github@tomaszklim.pl>
2018-07-07 15:12:34 +0200

    moved functions.net file to new sf-net-utils extension

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 . scripts/functions.custom
 . scripts/functions.dialog
 . scripts/functions.install
-. scripts/functions.net
 . scripts/functions.uid
 
 # This is a test script for Travis CI, to check critical files for syntax errors.
@@ -10,5 +9,4 @@
 HOST=`hostname`
 echo "hostname: $HOST"
 echo "external domain: `external_domain`"
-echo "google.com resolves to: `resolve_host google.com`"
 echo "first unused UID after 1000: `get_free_uid 1000 65000`"
