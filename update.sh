13ce74a27849d9bcc1f7dd9c255d95a6a99a6d73
Tomasz Klim <github@tomaszklim.pl>
2018-10-18 23:44:17 +0200

    added /opt/heartbeat to the list of always updated directories

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-update="/opt/farm `ls -d /opt/farm/ext/* 2>/dev/null`"
+update="/opt/heartbeat /opt/farm `ls -d /opt/farm/ext/* 2>/dev/null`"
 
 for PD in $update; do
 	/opt/farm/scripts/git/pull.sh $PD
