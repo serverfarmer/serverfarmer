bc6252f3098999392f5d2513585766f68f9aa8f1
Tomasz Klim <github@tomaszklim.pl>
2020-01-28 22:38:59 +0100

    clarifying comment

--- scripts/git/bootstrap.sh
+++ scripts/git/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# This script is responsible for installing Git client on various strange systems.
+# On standard systems, Git should be already available before Server Farmer core
+# is installed, since it is installed by cloning Git repository. However in some
+# cases, SF core can be installed from package.
 
+
+# Optware/ipkg (QNAP, DD-WRT etc. embedded distributions)
 if [ ! -x /bin/git ] && [ ! -x /opt/bin/git ] && [ ! -x /usr/bin/git ] && [ ! -x /usr/local/bin/git ] && [ -x /opt/bin/ipkg ]; then
 	echo "attempting to install Git"
 	/opt/bin/ipkg install git
